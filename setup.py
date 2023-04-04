3e803293aaa6f554d8de3498963e4565d3695baa
Ali Karbassi <ali@weallcode.org>
2023-04-04 15:38:38 -0500

    Running pre-commit (#10)

--- setup.py
+++ setup.py
@@ -1,21 +1,19 @@
 from setuptools import setup
 
 setup(
-    name='wacrobot',
-    version='0.1.0',    
-    description='A python package for controlling We All Code TinyBit Robots',
-    url='TODO:',
-    author='Blaine Rothrock',
-    author_email='blaine.p.rothrock@gmail.com',
-    license='TODO:',
-    packages=['wac-tinybit'],
-    install_requires=[
-        'bleak=0.2.0'
-    ],
+    name="wacrobot",
+    version="0.1.0",
+    description="A python package for controlling We All Code TinyBit Robots",
+    url="TODO:",
+    author="Blaine Rothrock",
+    author_email="blaine.p.rothrock@gmail.com",
+    license="TODO:",
+    packages=["wac-tinybit"],
+    install_requires=["bleak=0.2.0"],
     classifiers=[
-        'Development Status :: 1 - Planning', 
-        'Intended Audience :: Science/Research', # TODO
-        'License :: OSI Approved :: BSD License',  # TODO
-        'Programming Language :: Python :: 3.5', # TODO
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Science/Research",  # TODO
+        "License :: OSI Approved :: BSD License",  # TODO
+        "Programming Language :: Python :: 3.5",  # TODO
     ],
 )
