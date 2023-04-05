d286e8723d7c75ae290b82356809aedf0bdb55a6
Ali Karbassi <ali@weallcode.org>
2023-04-05 10:19:32 -0500

    Formatting

--- wacrobot/commands.py
+++ wacrobot/commands.py
@@ -3,6 +3,7 @@ from enum import Enum
 
 from bleak import BleakClient
 
+
 class RobotCommandType(Enum):
     LED = 1
     MOVE = 2
@@ -28,9 +29,9 @@ class LEDCommand(RobotCommand):
     def __init__(self, red: int, green: int, blue: int):
         super().__init__(RobotCommandType.LED)
 
-        self._service_uuid = '1A230001-C2ED-4D11-AD1E-FC06D8A02D37'
-        self._char_uuid = '1A230002-C2ED-4D11-AD1E-FC06D8A02D37'
-        
+        self._service_uuid = "1A230001-C2ED-4D11-AD1E-FC06D8A02D37"
+        self._char_uuid = "1A230002-C2ED-4D11-AD1E-FC06D8A02D37"
+
         # limit between 0 - 255
         self.red = min(255, max(0, red))
         self.green = min(255, max(0, green))
@@ -38,12 +39,12 @@ class LEDCommand(RobotCommand):
 
     def command(self):
         return bytes([self.red, self.green, self.blue])
-    
+
     async def execute(self, client: BleakClient):
-        print(f'led command: {self.command()}')
-        
+        print(f"led command: {self.command()}")
+
         service = client.services.get_service(self._service_uuid)
-        char = service.get_characteristic(self._char_uuid) 
+        char = service.get_characteristic(self._char_uuid)
 
     async def execute(self, client: BleakClient):
         print(f"led command: {self.command()}")
@@ -53,19 +54,19 @@ class LEDCommand(RobotCommand):
         print(f"led service: {service}, config char: {char}")
 
         await client.write_gatt_char(char, self.command(), response=True)
-        print('sent led command')
+        print("sent led command")
 
 
 class MoveCommand(RobotCommand):
     def __init__(self, left: int, right: int):
         super().__init__(RobotCommandType.MOVE)
 
-        self._service_uuid = '1A240001-C2ED-4D11-AD1E-FC06D8A02D37'
-        self._char_uuid = '1A240002-C2ED-4D11-AD1E-FC06D8A02D37'
+        self._service_uuid = "1A240001-C2ED-4D11-AD1E-FC06D8A02D37"
+        self._char_uuid = "1A240002-C2ED-4D11-AD1E-FC06D8A02D37"
 
         left = min(100, max(-100, left))
         right = min(100, max(-100, right))
-        
+
         self.left_fwd = left if left > 0 else 0
         self.left_rev = -left if left < 0 else 0
         self.right_fwd = right if right > 0 else 0
@@ -80,52 +81,56 @@ class MoveCommand(RobotCommand):
         char = service.get_characteristic(self._char_uuid)
 
         await client.write_gatt_char(char, self.command(), response=True)
-        print('sent wheels command')
+        print("sent wheels command")
 
 
 class DisplayTextCommand(RobotCommand):
     def __init__(self, text: str):
         super().__init__(RobotCommandType.LED)
 
-        self._service_uuid = '1A250001-C2ED-4D11-AD1E-FC06D8A02D37'
-        self._char_uuid = '1A250002-C2ED-4D11-AD1E-FC06D8A02D37'
+        self._service_uuid = "1A250001-C2ED-4D11-AD1E-FC06D8A02D37"
+        self._char_uuid = "1A250002-C2ED-4D11-AD1E-FC06D8A02D37"
 
         self.text = text
 
     def command(self):
-        return bytes([0x01] + list(self.text.encode('ascii')))
-    
+        return bytes([0x01] + list(self.text.encode("ascii")))
+
     async def execute(self, client: BleakClient):
         print(f"display text command: {self.text} (command: {self.command()})")
         service = client.services.get_service(self._service_uuid)
         char = service.get_characteristic(self._char_uuid)
 
         await client.write_gatt_char(char, self.command(), response=True)
-        print('sent display text command')
+        print("sent display text command")
+
 
 class DisplayDotMatrixCommand(RobotCommand):
-    def __init__(self, matrix: list[int]=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]):
+    def __init__(
+        self,
+        matrix: list[int] = [0] * 25,
+    ):
         super().__init__(RobotCommandType.LED)
 
-        self._service_uuid = '1A250001-C2ED-4D11-AD1E-FC06D8A02D37'
-        self._char_uuid = '1A250002-C2ED-4D11-AD1E-FC06D8A02D37'
+        self._service_uuid = "1A250001-C2ED-4D11-AD1E-FC06D8A02D37"
+        self._char_uuid = "1A250002-C2ED-4D11-AD1E-FC06D8A02D37"
 
         self.matrix = matrix
 
     def command(self):
         return bytes([0x02] + self.matrix)
-    
+
     async def execute(self, client: BleakClient):
         print(f"display text command: {self.matrix} (command: {self.command()})")
         service = client.services.get_service(self._service_uuid)
         char = service.get_characteristic(self._char_uuid)
 
         await client.write_gatt_char(char, self.command(), response=True)
-        print('sent display text command')
+        print("sent display text command")
 
 
 class WaitCommand(RobotCommand):
-    def __init__(self, duration: int):
+    def __init__(self, duration: float):
         super().__init__(RobotCommandType.WAIT)
         self.duration = duration
 
--- wacrobot/robot.py
+++ wacrobot/robot.py
@@ -1,8 +1,15 @@
 import asyncio
 import json
-from datetime import datetime, timedelta
-from .commands import RobotCommandType, RobotCommand, LEDCommand, MoveCommand, WaitCommand, DisplayTextCommand, DisplayDotMatrixCommand
+
 from bleak import BleakClient, BleakScanner
+
+from .commands import (
+    DisplayDotMatrixCommand,
+    DisplayTextCommand,
+    LEDCommand,
+    MoveCommand,
+    WaitCommand,
+)
 
 
 class Robot:
@@ -10,12 +17,12 @@ class Robot:
         self.name = name
         self.commands = []
 
-        with open('wacrobot/devices.json', 'r') as f:
+        with open("wacrobot/devices.json", "r") as f:
             self.device_map = json.load(f)
 
         if self.name not in self.device_map:
             self.address = name
-        else: 
+        else:
             self.address = self.device_map[self.name]
 
     def led(self, r, g, b):
@@ -25,35 +32,33 @@ class Robot:
         self.commands.append(MoveCommand(left, right))
 
     def stop(self):
-        self.commands.append(MoveCommand(0,0))
+        self.commands.append(MoveCommand(0, 0))
 
-    def wait(self, duration):
+    def wait(self, duration: float):
         self.commands.append(WaitCommand(duration))
 
     def displayText(self, text: str):
         self.commands.append(DisplayTextCommand(text))
-    
+
     def displayDots(self, matrix: list[int]):
         self.commands.append(DisplayDotMatrixCommand(matrix))
 
     def clearDisplay(self):
         self.commands.append(DisplayDotMatrixCommand())
 
-    async def _execute(self, client):
+    async def _execute(self, client: BleakClient):
         print(client)
         for i, command in enumerate(self.commands):
-            print(f'command {i}/{len(self.commands)}')
+            print(f"command {i}/{len(self.commands)}")
             await command.execute(client)
 
     async def _connect_and_run(self):
-        scanner = BleakScanner()
-
         async with BleakClient(self.address) as client:
             print("connected to device")
             await self._execute(client)
 
     def run(self):
         self.commands.append(DisplayDotMatrixCommand())
-        self.commands.append(MoveCommand(0,0))
-        self.commands.append(LEDCommand(0,0,0))
+        self.commands.append(MoveCommand(0, 0))
+        self.commands.append(LEDCommand(0, 0, 0))
         asyncio.run(self._connect_and_run())
